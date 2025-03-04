27d8ecf22cf8319b15306e4dda03eaf02cb84ea8
SkiFFFxx <Kim_lev12@mail.ru>
2025-03-04 14:44:48 +0600

    git add composer.phar

--- app/Http/Controllers/Fake/ParkController.php
+++ app/Http/Controllers/Fake/ParkController.php
@@ -10,13 +10,23 @@ class ParkController extends Controller
     // Получить список доступных парковочных мест
     public function getParkingSlots()
     {
+        $slots = [
+            ['id' => 1, 'name' => 'Slot A1', 'available' => true],
+            ['id' => 2, 'name' => 'Slot A2', 'available' => false],
+            ['id' => 3, 'name' => 'Slot B1', 'available' => true],
+        ];
+
+        // Перемешиваем порядок слотов случайным образом
+        shuffle($slots);
+
+        // Рандомно меняем доступность мест
+        foreach ($slots as &$slot) {
+            $slot['available'] = (bool)random_int(0, 1);
+        }
+
         return response()->json([
             'status' => 'success',
-            'slots' => [
-                ['id' => 1, 'name' => 'Slot A1', 'available' => true],
-                ['id' => 2, 'name' => 'Slot A2', 'available' => false],
-                ['id' => 3, 'name' => 'Slot B1', 'available' => true],
-            ]
+            'slots' => $slots
         ]);
     }
 
