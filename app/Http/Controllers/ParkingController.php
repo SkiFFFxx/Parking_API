39015d1a2d8c8721d73bee01d4108c064abeb6f2
SkiFFFxx <kim_lev12@mail.ru>
2025-03-10 21:30:30 +0500

    CRud сделал

--- app/Http/Controllers/ParkingController.php
+++ app/Http/Controllers/ParkingController.php
@@ -12,6 +12,7 @@ class ParkingController extends Controller
             'first' => '21:00',
             'second' => '00:00',
             'third' => '03:00',
+            'fourth' => '06:00',
         ];
         dd($parking_slots);
     }
